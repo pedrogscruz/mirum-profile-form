8951f6c5e9e291ad4f4c7e052d19aad46e250ed4
Unknown <pedro.gscruz@hotmail.com>
2019-04-08 00:34:01 -0300

    redux and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import { Home, Form, Profile } from './pages';
+
+// 'App' will manage redux and routes
+function App() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/form" component={Form} />
+				<Route path="/profile" component={Profile} />
+				
+				<Route component={() => <h1>404</h1>}/>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import reduxThunk from 'redux-thunk';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// importing the App component to pass it to ReactDOM
+import App from './App';
+
+const store = createStore(formReducer, {}, applyMiddleware(reduxThunk));
+
+// rendering the App with Redux
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
